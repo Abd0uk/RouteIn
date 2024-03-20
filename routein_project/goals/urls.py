ef0cdfa4284f725c6af9288a04b0d7a6a58bb7ed
Abd0uk <azkotb@gmail.com>
2024-03-20 17:17:06 +0200

    Creating the model for Goals

--- routein_project/goals/urls.py
+++ routein_project/goals/urls.py
@@ -4,5 +4,10 @@ from . import views
 app_name = 'goals'
 
 urlpatterns = [
-    path('create', views.GoalsCreateView.as_view(), name='create_goal')
+    # path('', views.GoalsListView.as_view(), name='list_goals'), List all
+    path('create', views.GoalsCreateView.as_view(), name='create_goal'), # create
+    # path('create', views.GoalsCreateView.as_view(), name='create_goal'), # list 1
+    # path('create', views.GoalsCreateView.as_view(), name='create_goal'), # update
+    # path('create', views.GoalsCreateView.as_view(), name='create_goal'), # delete
+    
 ]
